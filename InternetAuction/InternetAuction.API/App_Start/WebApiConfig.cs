7d0aeb1550c972c01ff78c876ef527305871c222
Jan Markovich <markovich.jan@gmail.com>
2016-09-11 16:26:34 +0300

    CORS enabled.

--- InternetAuction/InternetAuction.API/App_Start/WebApiConfig.cs
+++ InternetAuction/InternetAuction.API/App_Start/WebApiConfig.cs
@@ -11,6 +11,8 @@ namespace InternetAuction.API
         {
             // Web API configuration and services
 
+            config.EnableCors();
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
