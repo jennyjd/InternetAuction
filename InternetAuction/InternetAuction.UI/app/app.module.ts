1d5d288679623eacfc14c179a1e01de39c3c3117
Evgenia Yavorskaya <night994@gmail.com>
2016-10-11 22:23:05 +0300

    Created base structure

---
+++ InternetAuction/InternetAuction.UI/app/app.module.ts
@@ -0,0 +1,22 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+import { MdCardModule } from '@angular2-material/card';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdListModule } from '@angular2-material/list';
+import { MdInputModule } from '@angular2-material/input';
+
+import { MdIconRegistry } from '@angular2-material/icon';
+
+@NgModule({
+    imports: [BrowserModule, MdCardModule, MdButtonModule, MdIconModule, MdSidenavModule, MdToolbarModule,
+        MdListModule, MdInputModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    providers: [MdIconRegistry]
+})
+export class AppModule { }
