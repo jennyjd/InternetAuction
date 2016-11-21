4aa97b3ef181cb9dd49cdbd56fe9428e76b382fb
Evgenia Yavorskaya <night994@gmail.com>
2016-11-21 18:06:08 +0300

    Tested LotService (get lot by id)

--- InternetAuction/InternetAuction.UI/app/lot/lot-detail/lot-detail.component.ts
+++ InternetAuction/InternetAuction.UI/app/lot/lot-detail/lot-detail.component.ts
@@ -2,20 +2,26 @@
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 
+import { LotService } from '../lot.service';
+
 @Component({
     selector: 'lot-detail',
     templateUrl: './app/lot/lot-detail/lot-detail.component.html',
-    styleUrls: ['./app/lot/lot-detail/lot-detail.component.css']
+    styleUrls: ['./app/lot/lot-detail/lot-detail.component.css'],
+    providers: [LotService]
 })
 
 export class LotDetailComponent implements OnInit {
+    errorMessage: any;
 
-    constructor(private route: ActivatedRoute, private router: Router) { }
+    constructor(private route: ActivatedRoute, private router: Router, private lotservise: LotService) { }
 
     ngOnInit() {
-        /*this.route.params
-            // (+) converts string 'id' to a number
-            .switchMap((params: Params) => this.service.getHero(+params['id']))
-            .subscribe((hero: Hero) => this.hero = hero);*/
+        this.route.params
+            .switchMap((params: Params) => this.lotservise.getLotById(+params['id']))
+            .subscribe(res => {
+                console.log(res);
+            },
+            error => this.errorMessage = <any>error);
     }
 }
--- InternetAuction/InternetAuction.UI/app/lot/lot.service.ts
+++ InternetAuction/InternetAuction.UI/app/lot/lot.service.ts
@@ -1,7 +1,7 @@
 ﻿import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Router } from '@angular/router';
-import { Headers, RequestOptions } from '@angular/http';
+import { Headers, RequestOptions, RequestMethod, Request} from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 
@@ -11,8 +11,19 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class LotService {
-    private getLotsUrl = 'http://localhost:21561/api/auctionCategories/GetAuctionCategories';
+    private getLotsUrl = 'http://localhost:21561/api/auctions/getAuction';
 
     constructor(private http: Http) {
+    }
+
+    getLotById(id) {
+        return this.http.get(`${this.getLotsUrl}/${id}`)
+            .map(response => response.json())
+            .catch(this.handleError);
+    }
+
+    public handleError(error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
     }
 }
