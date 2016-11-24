d5a92c9ad28e587b202c5b0f3aa1c23d181e96dc
Jan Markovich <markovich.jan@gmail.com>
2016-11-25 00:50:41 +0300

    Swagger via Swashbuckle enabled on URL: ~/swagger

---
+++ InternetAuction/InternetAuction.API/App_Start/SwaggerConfig.cs
@@ -0,0 +1,15 @@
+﻿using Swashbuckle.Application;
+using System.Web.Http;
+
+namespace InternetAuction.API
+{
+    public static class SwaggerConfig
+    {
+        public static void RegisterSwagger(HttpConfiguration config)
+        {
+            config
+                .EnableSwagger(c => c.SingleApiVersion("v1", "InternetAuction API"))
+                .EnableSwaggerUi();
+        }
+    }
+}
