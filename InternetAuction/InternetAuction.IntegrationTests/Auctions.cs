b2758df89da38b50dc2e0d8e008a561c4bc5f0e4
Jan Markovich <markovich.jan@gmail.com>
2016-12-11 16:17:33 +0300

    Added integration tests.

---
+++ InternetAuction/InternetAuction.IntegrationTests/Auctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InternetAuction.IntegrationTests
+{
+    [TestClass]
+    public class Auctions
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ InternetAuction/InternetAuction.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using InternetAuction.API.ViewModels;
+
+namespace InternetAuction.IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync("http://localhost/InternetAuctionApi/api/testapi/testmsg").Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+
+
+
+            var content1 = JsonConvert.SerializeObject(new LoginModelVM
+            {
+                UserName = "Ivan",
+                Password = "IvanIvan"
+            }); 
+            response = client.PostAsync("http://localhost/InternetAuctionApi/api/account/signin", new StringContent(content1, Encoding.UTF8, "application/json")).Result;
+
+            content = response.Content.ReadAsStringAsync().Result;
+
+            response = client.GetAsync("http://localhost/InternetAuctionApi/api/testapi/testauthorize").Result;
+            content = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(content);
+        }
+    }
+}
