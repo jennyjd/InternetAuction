354280b17075e8370b37ec5e20327b61fa14f9f5
Evgenia Yavorskaya <night994@gmail.com>
2016-12-21 00:31:23 +0300

    Bet validation fixed

--- InternetAuction/InternetAuction.UI/app/lot/lot-detail/lot-detail.component.ts
+++ InternetAuction/InternetAuction.UI/app/lot/lot-detail/lot-detail.component.ts
@@ -90,15 +90,23 @@ export class LotDetailComponent implements OnInit, AfterViewInit {
         let bet = parseInt(sum.model);
         this.betAmmountErrors = [];
         console.log(bet);
+        console.log(this.selected_lot.PriceOfFastSell);
         console.log(this.selected_lot.currentBet);
-        if (bet < this.selected_lot.currentBet) {
-            this.betAmmountErrors.push("Ваша ставка меньше текущей!");
+        if (this.selected_lot.currentBet != 0) {
+            if (bet <= this.selected_lot.currentBet) {
+                this.betAmmountErrors.push("Ваша ставка должна быть больше текущей!");
+            }
         }
-        else if (bet > this.selected_lot.PriceOfFastSell) {
-            this.betAmmountErrors.push("Ваша ставка больше стоимости выкупа! Чтобы выкупить данный лот, нажмите на кнопку 'Выкупить'");
+        else if (bet <= this.selected_lot.StartPrice) {
+            this.betAmmountErrors.push("Ваша ставка должна быть больше начальной!");
         }
-        else if (bet == this.selected_lot.PriceOfFastSell) {
-            this.betAmmountErrors.push("Ваша ставка cоответствует стоимости выкупа! Чтобы выкупить данный лот, нажмите на кнопку 'Выкупить'");
+        else if (this.selected_lot.PriceOfFastSell != null) {
+            if (bet > this.selected_lot.PriceOfFastSell) {
+                this.betAmmountErrors.push("Ваша ставка больше стоимости выкупа! Чтобы выкупить данный лот, нажмите на кнопку 'Выкупить'");
+            }
+            else if (bet == this.selected_lot.PriceOfFastSell) {
+                this.betAmmountErrors.push("Ваша ставка cоответствует стоимости выкупа! Чтобы выкупить данный лот, нажмите на кнопку 'Выкупить'");
+            }
         }
         else {
             this.changeModal();
