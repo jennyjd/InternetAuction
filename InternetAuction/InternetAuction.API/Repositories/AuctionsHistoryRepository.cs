ae94babe65f27bc71fa33916b04ac602945d7b18
Jan Markovich <markovich.jan@gmail.com>
2016-12-21 18:08:32 +0300

    Hot fix.

--- InternetAuction/InternetAuction.API/Repositories/AuctionsHistoryRepository.cs
+++ InternetAuction/InternetAuction.API/Repositories/AuctionsHistoryRepository.cs
@@ -99,7 +99,7 @@ namespace InternetAuction.API.Repositories
                     AuctionId = auction.Id,
                     IsCompleted = auction.IsCompleted,
                     MaxBet = CheckCurrentMaxBet(auction.Id),
-                    CustomerId = _context.AuctionsHistory.Where(x => x.AuctionId == auction.Id).OrderBy(x => x.Date).AsEnumerable().LastOrDefault().ClientId
+                    CustomerId = _context.AuctionsHistory.Where(x => x.AuctionId == auction.Id).OrderBy(x => x.Date).AsEnumerable().LastOrDefault()?.ClientId
                 });
             }
 
